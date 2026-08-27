c9616cb0b537fb3a3f7defb330a998d42fa92a62
songjiang <songjiang@local>
2026-08-27 19:50:31 +0800

    feat: rebrand anx-reader as songjiang_reader (松江阅)

---
+++ lib/dao/theme.dart
@@ -0,0 +1,65 @@
+import 'package:songjiang_reader/dao/base_dao.dart';
+import 'package:songjiang_reader/l10n/generated/L10n.dart';
+import 'package:songjiang_reader/main.dart';
+import 'package:songjiang_reader/models/read_theme.dart';
+import 'package:songjiang_reader/utils/toast/common.dart';
+
+class ThemeDao extends BaseDao {
+  ThemeDao();
+
+  static const String table = 'tb_themes';
+
+  Future<int> insertTheme(ReadTheme readTheme) {
+    return insert(table, readTheme.toMap());
+  }
+
+  Future<List<ReadTheme>> selectThemes() {
+    return queryList(
+      table,
+      mapper: ReadTheme.fromDb,
+    );
+  }
+
+  Future<void> deleteTheme(int id) async {
+    final currentThemes = await queryList(
+      table,
+      mapper: ReadTheme.fromDb,
+    );
+    if (currentThemes.length <= 2) {
+      AnxToast.show(
+          L10n.of(navigatorKey.currentContext!).readingPageAtLeastTwoThemes);
+      return;
+    }
+
+    await delete(
+      table,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  Future<void> updateTheme(ReadTheme readTheme) async {
+    await update(
+      table,
+      readTheme.toMap(),
+      where: 'id = ?',
+      whereArgs: [readTheme.id],
+    );
+  }
+
+  Future<ReadTheme> selectReadThemeById(int id) async {
+    final theme = await querySingle(
+      table,
+      mapper: ReadTheme.fromDb,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (theme == null) {
+      throw StateError('Theme with id $id not found');
+    }
+    return theme;
+  }
+}
+
+final themeDao = ThemeDao();
---
+++ lib/service/font.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:songjiang_reader/l10n/generated/L10n.dart';
+import 'package:songjiang_reader/main.dart';
+import 'package:songjiang_reader/utils/get_path/get_base_path.dart';
+import 'package:songjiang_reader/utils/toast/common.dart';
+import 'package:file_picker/file_picker.dart';
+
+Future<void> importFont() async {
+  FilePickerResult? result = await FilePicker.platform.pickFiles(
+    type: FileType.custom,
+    allowedExtensions: ['ttf', 'otf'],
+    allowMultiple: true,
+  );
+
+  if (result == null) {
+    return;
+  }
+
+  List<PlatformFile> files = result.files;
+  for (var file in files) {
+    final fontDir = getFontDir();
+    File newFile = File(file.path!);
+    newFile.copy('${fontDir.path}/${file.name}');
+
+    AnxToast.show(L10n.of(navigatorKey.currentContext!).commonSuccess);
+  }
+}
